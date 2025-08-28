250dd9870b0856caffcfa385b24e74635119d8e1
Rafael <rafaelpierotti236@gmail.com>
2025-08-28 17:43:46 -0300

    Inicio do Jogo

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("JOGO_POO")
+import pygame
+from pygame.locals import *
+from sys import exit
+from random import randint
+
+pygame.init()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT: # para sair
+            pygame.quit()
+            exit()
+    pygame.display.update()
